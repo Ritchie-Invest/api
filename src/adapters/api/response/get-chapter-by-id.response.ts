5d286a1b15a114a6becb825f06a68a08ac8e11ea
MisterAzix <flizi@live.fr>
2025-06-06 11:36:17 +0200

    feat: adding basic chapters admin CRUD ✨

---
+++ src/adapters/api/response/get-chapter-by-id.response.ts
@@ -0,0 +1,37 @@
+import { ApiProperty } from '@nestjs/swagger';
+
+export class GetChapterByIdResponse {
+  @ApiProperty()
+  id: string;
+
+  @ApiProperty()
+  title: string;
+
+  @ApiProperty()
+  description: string;
+
+  @ApiProperty()
+  is_published: boolean;
+
+  @ApiProperty()
+  updatedAt: Date;
+
+  @ApiProperty()
+  createdAt: Date;
+
+  constructor(
+    id: string,
+    title: string,
+    description: string,
+    is_published: boolean,
+    updatedAt: Date,
+    createdAt: Date,
+  ) {
+    this.id = id;
+    this.title = title;
+    this.description = description;
+    this.is_published = is_published;
+    this.updatedAt = updatedAt;
+    this.createdAt = createdAt;
+  }
+}
---
+++ src/core/domain/repository/chapter.repository.ts
@@ -0,0 +1,4 @@
+import { Repository } from '../../base/repository';
+import { Chapter } from '../model/Chapter';
+
+export abstract class ChapterRepository extends Repository<Chapter> {}
