c3df18a787b8b97a0e35d12221fcc34c32b379c3
Charles Lambret <79523370+CharlesLambret@users.noreply.github.com>
2025-09-05 18:58:16 +0200

    fix: lint

--- prisma/migrations/20250904214503_true_or_false/migration.sql
+++ prisma/migrations/20250904214503_true_or_false/migration.sql
@@ -2,14 +2,11 @@
 
 ALTER TYPE "GameType" ADD VALUE 'TRUE_OR_FALSE';
 
--- CreateTable
 CREATE TABLE "TrueOrFalseModule" (
     "questions" JSONB NOT NULL,
     "gameModuleId" TEXT NOT NULL
 );
 
--- CreateIndex
 CREATE UNIQUE INDEX "TrueOrFalseModule_gameModuleId_key" ON "TrueOrFalseModule"("gameModuleId");
 
--- AddForeignKey
 ALTER TABLE "TrueOrFalseModule" ADD CONSTRAINT "TrueOrFalseModule_gameModuleId_fkey" FOREIGN KEY ("gameModuleId") REFERENCES "GameModule"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
